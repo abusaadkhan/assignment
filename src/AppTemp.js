4eefede931233b385ba51cebae1616fe39bc8109
abusaadkhan <abu70179@gmail.com>
2022-03-07 10:29:26 +0530

    '.'

--- src/AppTemp.js
+++ src/AppTemp.js
@@ -15,23 +15,22 @@ function App() {
  const [posts,setPosts] = useState([])
   const [layout,setLayout] = useState('grid')
   const[containerBlur,setContainerBlur] = useState('containerBlur')
+
   const [showPerPage, setShowPerPage] = useState(6);
   const [pagination, setPagination] = useState({
     start: 0,
     end: showPerPage,
   });
-  const [openModal,setOpenModal] = useState(false)
-  const [modalShow, setModalShow] = useState(false);
-
-  const onPaginationChange = (start, end) => {
-    setPagination({ start: start, end: end });
-  };
   
   const fetchPosts = async() => {
-    const response = await Axios.get(' http://api.mediastack.com/v1/news?access_key=985f363243c57c561b4a248d13962cc9&countries=in&limit=30&offset=2')
+    const response = await Axios.get(' https://api.mediastack.com/v1/news?access_key=985f363243c57c561b4a248d13962cc9&countries=in&limit=30&offset=2')
     setPosts(response.data.data)
     console.log('data stored in post:',posts)
    }
+
+   const onPaginationChange = (start, end) => {
+    setPagination({ start: start, end: end });
+  };
 
   const changeLayout = () =>{
     setLayout(layout==='grid'? 'list' : 'grid')
